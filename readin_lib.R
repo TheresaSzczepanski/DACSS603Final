e439eb0d3ad3fc00dd816baee344b5aa99b2e677
Theresa Szczepanski <theresaszczepanski@gmail.com>
2023-10-15 11:26:19 -0400

    Updated Data Cleaning with State Item Analysis Reports

--- readin_lib.R
+++ readin_lib.R
@@ -2,42 +2,49 @@ read_item_XWalk<-function(file_path, sheet_name){
   read_excel(file_path, sheet = sheet_name)
 }
 
-
-read_state_item<-function(file_path, sheet_name, year, subject){
-  if(subject == "math"){
-    State_Percent<-read_excel(file_path, sheet = sheet_name, 
-                              skip = 1) %>%
-      select(!contains("Subject"))%>%
-      pivot_longer(!(contains("District")|contains("Tested")), names_to = "ITEM", values_to = "District%")%>%
-      mutate(ITEM = as.integer(ITEM))%>%
-      mutate(Tested = as.integer(Tested))%>%
+# read_school_item<-function(file_path, year, subject){
+#   
+# }
+# subject should be PHY or BIO
+read_state_item<-function(file_path, year, subject){
+  if(subject == "PHY"){
+    State_Percent<-read_excel(file_path, skip = 1)%>%
+    select(!contains("Subject"))%>%
+    pivot_longer(!(contains("District")|contains("Tested")|contains("Rate")), names_to = "ITEM", values_to = "District%")%>%
+     mutate(ITEM = as.integer(ITEM))%>%
+     mutate(Tested = as.integer(Tested))%>%
       filter(str_detect(`District Name`,"State"))%>%
       mutate(`State%` = `District%`)%>%
-      select(`ITEM`, `State%`)
+      select(`ITEM`, `State%`)%>%
+      mutate(`Year` = year)%>%
+      mutate(`Subject` = subject)
     view(State_Percent)
-    if(year == 2019){
-      read_excel(file_path, sheet = sheet_name, 
-                 skip = 1) %>%
-        select(!contains("Subject"))%>%
-        pivot_longer(!(contains("District")|contains("Tested")), names_to = "ITEM", values_to = "District%")%>%
-        mutate(ITEM = as.integer(ITEM))%>%
-        mutate(Tested = as.integer(Tested))%>%
-        filter(!str_detect(`District Name`,"State"))%>%
-        left_join(State_Percent, "ITEM")%>%
-        mutate(`District-State Diff` = `District%`-`State%`)
-      
-    }else if(year > 2019){
-      read_excel(file_path, sheet = sheet_name, 
-                 skip = 1) %>%
-        select(!contains("Subject"))%>%
-        select(!contains("Part."))%>%
-        pivot_longer(!(contains("District")|contains("Tested")), names_to = "ITEM", values_to = "District%")%>%
-        mutate(ITEM = as.integer(ITEM))%>%
-        filter(!str_detect(`District Name`,"State"))%>%
-        left_join(State_Percent, "ITEM")%>%
-        mutate(`District-State Diff` = `District%`-`State%`)
-      
-    }
+    
+    # if(year > 2019){
+    #   read_excel(file_path,
+    #              skip = 1) %>%
+    #     select(!contains("Subject"))%>%
+    #     select(!contains("Part."))%>%
+    #     pivot_longer(!(contains("District")|contains("Tested")), names_to = "ITEM", values_to = "District%")%>%
+    #     mutate(ITEM = as.integer(ITEM))%>%
+    #     filter(!str_detect(`District Name`,"State"))%>%
+    #     left_join(State_Percent, "ITEM")%>%
+    #     mutate(`District-State Diff` = `District%`-`State%`)
+    #   
+    # }
+    # 
+    # else if(year == 2019){
+    #   read_excel(file_path,
+    #              skip = 1) %>%
+    #     select(!contains("Subject"))%>%
+    #     pivot_longer(!(contains("District")|contains("Tested")), names_to = "ITEM", values_to = "District%")%>%
+    #     mutate(ITEM = as.integer(ITEM))%>%
+    #     mutate(Tested = as.integer(Tested))%>%
+    #     filter(!str_detect(`District Name`,"State"))%>%
+    #     left_join(State_Percent, "ITEM")%>%
+    #     mutate(`District-State Diff` = `District%`-`State%`)
+    #   
+    # }
   }else if(subject == "ELA"){
     # State_Percent<-read_excel(file_path, sheet = sheet_name, 
     #                           skip = 1) %>%
@@ -53,7 +60,7 @@ read_state_item<-function(file_path, sheet_name, year, subject){
     #   select(`ITEM`, `State %`)
     # view(State_Percent)
     if(year == 2019){
-      read_excel(file_path, sheet = sheet_name, 
+      read_excel(file_path,  
                  skip = 1) %>%
         select(!contains("Subject"))%>%
         pivot_longer(!(contains("District")|contains("Tested")), names_to = "ITEM", values_to = "District%")%>%
