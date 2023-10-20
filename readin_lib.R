4a19ca99a838e96d9f6ed6c93e7d2c92f10337f5
Theresa Szczepanski <theresaszczepanski@gmail.com>
2023-10-20 11:38:27 -0400

    updated, wider state performance data frame.
    ToDo: How do you summarize the information accross the years?

--- readin_lib.R
+++ readin_lib.R
@@ -34,74 +34,96 @@ read_IT301 <- function(file_path, year, subject){
 read_state_achievement<-function(file_path, year, subject){
   if(subject == "PHY"){
     state_perf_sum <-read_excel(file_path, skip = 1, col_names = c("delete", "School Code", "Subject", 
-                                                                   "delete", "delete", "delete", "E%", 
-                                                                   "delete", "M%", "delete",
-                                                                   "PM%", "delete", "NM%", 
+                                                                   "delete", "delete", "E#", "E%", 
+                                                                   "M#", "M%", "PM#",
+                                                                   "PM%", "NM#", "NM%", 
                                                                    "delete", "delete",
                                                                    "State Avg. Scaled Score", 
                                                                    "delete", "delete"))%>%
       select(!contains("delete"))%>%
       filter(`Subject`== subject) %>%
       filter(`School Code` == "00000000")%>%
-      pivot_longer(contains("%"), names_to = "Performance Level", values_to = "State Performance%")%>%
-      mutate(`Performance Level` = case_when(
-        `Performance Level` == "E%" ~ "E",
-        `Performance Level` == "M%" ~ "M",
-        `Performance Level` == "PM%" ~ "PM",
-        `Performance Level` == "NM%" ~ "NM"
-      ))%>%
-      mutate(`Performance Level` = recode_factor(`Performance Level`,
-                                                 "E" = "E",
-                                                 "M" = "M",
-                                                 "PM" = "PM",
-                                                 "NM" = "NM",
-                                                 .ordered = TRUE))%>%
+      # pivot_longer(contains("%"), names_to = "Performance Level", values_to = "State Performance%")%>%
+      #mutate(`Tested Students` = as.numeric(`Tested Students`))%>%
+      mutate(`E#` = as.integer(gsub(",", "", `E#`)))%>%
+      mutate(`E%` = as.numeric(`E%`))%>%
+      mutate(`M#` = as.integer(gsub(",", "", `M#`)))%>%
+      mutate(`M%` = as.numeric(`M%`))%>%
+      mutate(`PM#` = as.integer(gsub(",", "", `PM#`)))%>%
+      mutate(`PM%` = as.numeric(`PM%`))%>%
+      mutate(`NM#` = as.integer(gsub(",", "", `NM#`)))%>%
+      mutate(`NM%` = as.numeric(`NM%`))%>%
+      mutate(`E%State` = `E%`)%>%
+      mutate(`E#State` = `E#`)%>%
+      mutate(`M%State` = `M%`)%>%
+      mutate(`M#State` = `M#`)%>%
+      mutate(`PM%State` = `PM%`)%>%
+      mutate(`PM#State` = `PM#`)%>%
+      mutate(`NM%State` = `NM%`)%>%
+      mutate(`NM#State` = `NM#`)%>%
+        # `Performance Level` == "E%" ~ "E",
+      #   `Performance Level` == "M%" ~ "M",
+      #   `Performance Level` == "PM%" ~ "PM",
+      #   `Performance Level` == "NM%" ~ "NM"
+      # ))%>%
+      # mutate(`Performance Level` = recode_factor(`Performance Level`,
+      #                                            "E" = "E",
+      #                                            "M" = "M",
+      #                                            "PM" = "PM",
+      #                                            "NM" = "NM",
+      #                                            .ordered = TRUE))%>%
       mutate(`State Avg. Scaled Score` = as.numeric(`State Avg. Scaled Score`))%>%
-      select(`Performance Level`, `State Performance%`, `State Avg. Scaled Score`)
-    #view(state_perf_sum)
+      mutate(`Year` = year)%>%
+      mutate(`State Tested` = `E#State` + `M#State` + `PM#State` + `NM#State`)%>%
+      select(`Year`, `State Tested`, `E#State`, `E%State`, `M#State`, `M%State`, 
+             `PM#State`, `PM%State`, `NM#State`, `NM%State`, `State Avg. Scaled Score`)
+    view(state_perf_sum)
     read_excel(file_path, skip = 1, col_names = c("School Name", "School Code", "Subject", 
-                                                  "delete", "delete", "E Count", "delete", 
-                                                  "M Count", "delete", "PM Count",
-                                                  "delete", "NM Count", "delete", 
+                                                  "delete", "delete", "E#", "E%", 
+                                                  "M#", "M%", "PM#",
+                                                  "PM%", "NM#", "NM%", 
                                                   "Tested Students", "delete",
                                                   "Avg. Scaled Score", "delete", "delete"))%>%
       select(!contains("delete"))%>%
       #mutate(`M+E Count` = as.integer(`M+E Count`))%>%
       #mutate(`M+E%` = as.numeric(`M+E%`))%>%
-      mutate(`E Count` = as.integer(`E Count`))%>%
-      #mutate(`E%` = as.numeric(`E%`))%>%
-      mutate(`M Count` = as.integer(`M Count`))%>%
-      #mutate(`M%` = as.numeric(`M%`))%>%
-      mutate(`PM Count` = as.integer(`PM Count`))%>%
-      #mutate(`PM%` = as.numeric(`PM%`))%>%
-      mutate(`NM Count` = as.integer(`NM Count`))%>%
-      #mutate(`NM%` = as.numeric(`NM%`))%>%
+      mutate(`E#` = as.integer(`E#`))%>%
+      mutate(`E%` = as.numeric(`E%`))%>%
+      mutate(`M#` = as.integer(`M#`))%>%
+      mutate(`M%` = as.numeric(`M%`))%>%
+      mutate(`PM#` = as.integer(`PM#`))%>%
+      mutate(`PM%` = as.numeric(`PM%`))%>%
+      mutate(`NM#` = as.integer(`NM#`))%>%
+      mutate(`NM%` = as.numeric(`NM%`))%>%
       mutate(`Tested Students` = as.integer(`Tested Students`))%>%
       mutate(`Avg. Scaled Score` = as.integer(`Avg. Scaled Score`))%>%
       mutate(`Year` = year)%>%
       filter(`Subject`== subject)%>%
-      pivot_longer(contains("Count"), names_to = "Performance Level", values_to = "Performance Count")%>%
-      mutate(`Performance Level` = case_when(
-                    `Performance Level` == "E Count" ~ "E",
-                    `Performance Level` == "M Count" ~ "M",
-                    `Performance Level` == "PM Count" ~ "PM",
-                    `Performance Level` == "NM Count" ~ "NM"
-      ))%>%
-      mutate(`Performance Level` = recode_factor(`Performance Level`,
-                                                 "E" = "E",
-                                                 "M" = "M",
-                                                 "PM" = "PM",
-                                                 "NM" = "NM",
-                                                 .ordered = TRUE))%>%
-      mutate(`Performance%` = round(100*`Performance Count`/`Tested Students`))%>%
-      mutate(`State Tested` = sum(`Tested Students`))%>%
-      select(`Year`, `Subject`, `School Name`, `School Code`, `Tested Students`, `Performance Level`, 
-             `Performance Count`, `Performance%`, `Avg. Scaled Score`)%>%
-      left_join(state_perf_sum, by= "Performance Level")%>%
-      mutate(`State Performance%` = as.numeric(`State Performance%`))%>%
-      mutate(`Performance Diff` = `Performance%` - `State Performance%`)%>%
+      # pivot_longer(contains("Count"), names_to = "Performance Level", values_to = "Performance Count")%>%
+      # mutate(`Performance Level` = case_when(
+      #               `Performance Level` == "E Count" ~ "E",
+      #               `Performance Level` == "M Count" ~ "M",
+      #               `Performance Level` == "PM Count" ~ "PM",
+      #               `Performance Level` == "NM Count" ~ "NM"
+      # ))%>%
+      # mutate(`Performance Level` = recode_factor(`Performance Level`,
+      #                                            "E" = "E",
+      #                                            "M" = "M",
+      #                                            "PM" = "PM",
+      #                                            "NM" = "NM",
+      #                                            .ordered = TRUE))%>%
+      # mutate(`Performance%` = round(100*`Performance Count`/`Tested Students`))%>%
+      # mutate(`State Tested` = sum(`Tested Students`))%>%
+       select(`Year`, `Subject`, `School Name`, `School Code`, `Tested Students`, 
+              `E#`, `E%`, `M#`, `M%`, `PM#`, `PM%`, `NM#`, `NM%`,`Avg. Scaled Score`)%>%
+       #       `Performance Count`, `Performance%`, `Avg. Scaled Score`)%>%
+      left_join(state_perf_sum, by= "Year")%>%
+     # mutate(`State Performance%` = as.numeric(`State Performance%`))%>%
+      #mutate(`Performance Diff` = `Performance%` - `State Performance%`)%>%
       select(`Year`, `Subject`, `School Name`, `School Code`, `Tested Students`, 
-             `Performance Level`, `Performance Count`, `Performance%`, `State Performance%`, `Performance Diff`,
+             `E#`, `E%`, `M#`, `M%`, `PM#`, `PM%`, `NM#`, `NM%`, 
+             `E#State`, `E%State`, `M#State`, `M%State`, `PM#State`, `PM%State`, 
+             `NM#State`, `NM%State`,
              `Avg. Scaled Score`, `State Avg. Scaled Score`)%>%
       filter(`School Code` != "00000000")
     
@@ -198,3 +220,4 @@ read_state_item<-function(file_path, year, subject){
     }
   }
 }
+
